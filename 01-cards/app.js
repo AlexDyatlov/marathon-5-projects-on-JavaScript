41b9940d7792d014eb9a8c08534af4d2829f68a5
AlexDyatlov <aleks78.gooogle@gmail.com>
2021-08-19 19:04:32 +0300

    finished project 1, started project 2

---
+++ 01-cards/app.js
@@ -0,0 +1,21 @@
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide');
+
+  slides[activeSlide].classList.add('slide--active');
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+
+      slide.classList.add('slide--active');
+    });
+  };
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('slide--active')
+    });
+  };
+};
+
+slidesPlugin(3);
